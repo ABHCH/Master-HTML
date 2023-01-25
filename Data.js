f53f2b2ed9a30d725ed362e3cf72cba1c028aac1
ABHCH <chaturvedia435@gmail.com>
2023-01-25 22:49:34 +0530

    FILTER MOVIES APP

---
+++ Data.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        id:1,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/d9nBoowhjiiYc4FBNtQkPY7c11H.jpg',
+        imgName:'Megan'
+    },
+    {
+        id:2,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/62HCnUTziyWcpDaBO2i1DX17ljH.jpg',
+        imgName:'Top Gun'
+    },
+    {
+        id:3,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/r7ZipEd9oHHVVXTcvbx3Sd3Tukv.jpg',
+        imgName:'Candy land'
+    },
+    {
+        id:4,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/dPV6iMhltNaU7sOTLm9ytkxlQ1U.jpg',
+        imgName:'Pathan'
+    },
+    {
+        id:5,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/wjOHjWCUE0YzDiEzKv8AfqHj3ir.jpg',
+        imgName:'BabyLon'
+    },
+    {
+        id:6,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/w3LxiVYdWWRvEVdn5RYq6jIqkb1.jpg',
+        imgName:'Everything ever'
+    },
+    {
+        id:7,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg',
+        imgName:'Avatar'
+    },
+    {
+        id:8,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/gU4mmINWUF294Wzi8mqRvi6peMe.jpg',
+        imgName:'Narvik'
+    },
+    {
+        id:9,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/z2nfRxZCGFgAnVhb9pZO87TyTX5.jpg',
+        imgName:'Jungle'
+    },
+    {
+        id:10,
+        image:'https://www.themoviedb.org/t/p/w220_and_h330_face/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg',
+        imgName:'the Last of us'
+    },
+]
